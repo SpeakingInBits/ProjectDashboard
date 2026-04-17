f785443659e7e707e4856095e00896025f378685
joeprogrammer88 <joeProgrammer88@users.noreply.github.com>
2026-04-16 20:14:28 -0700

    Add pointer hover cursor for repo label on Windows
    Adds PointerGestureRecognizer handlers to the repository label to change the mouse cursor on pointer enter/exit (hand/arrow) for Windows. Implements OnRepoLabelPointerEntered/Exited in MainPage.xaml.cs and introduces CursorHelper.cs to set the protected UIElement.ProtectedCursor property via reflection.

--- ProjectDashboard/MainPage.xaml.cs
+++ ProjectDashboard/MainPage.xaml.cs
@@ -21,10 +21,38 @@ namespace ProjectDashboard
 
         private async void OnReorderCompleted(object sender, EventArgs e)
         {
-            // CollectionView has already updated the ObservableCollection;
-            // just persist the new order to SQLite.
             await _viewModel.PersistCurrentSortOrderAsync();
         }
+
+        private void OnRepoLabelPointerEntered(object sender, PointerEventArgs e)
+        {
+#if WINDOWS
+            if (sender is VisualElement element &&
+                element.Handler?.PlatformView is Microsoft.UI.Xaml.UIElement native)
+            {
+                SetCursor(native, Microsoft.UI.Input.InputSystemCursorShape.Hand);
+            }
+#endif
+        }
+
+        private void OnRepoLabelPointerExited(object sender, PointerEventArgs e)
+        {
+#if WINDOWS
+            if (sender is VisualElement element &&
+                element.Handler?.PlatformView is Microsoft.UI.Xaml.UIElement native)
+            {
+                SetCursor(native, Microsoft.UI.Input.InputSystemCursorShape.Arrow);
+            }
+#endif
+        }
+
+#if WINDOWS
+        private static void SetCursor(Microsoft.UI.Xaml.UIElement element, Microsoft.UI.Input.InputSystemCursorShape shape)
+        {
+            // UIElement.ProtectedCursor is protected, so we use a helper subclass to set it.
+            CursorHelper.SetCursor(element, Microsoft.UI.Input.InputSystemCursor.Create(shape));
+        }
+#endif
     }
 }
 
